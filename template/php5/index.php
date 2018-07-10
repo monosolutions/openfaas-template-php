da7842a73d47b34524812d7e2ef22bfebf6cc125
Magnus Udbjørg <magnus@udbjorg.net>
2018-07-10 15:14:17 +0200

    allowing multi-line stdin

--- template/php5/index.php
+++ template/php5/index.php
@@ -1,5 +1,8 @@
 <?php
-
+function isValidJson($strJson) { 
+    json_decode($strJson); 
+    return (json_last_error() === JSON_ERROR_NONE); 
+} 
 // Requires Core composer's autoload
 if (file_exists('vendor/autoload.php')) {
     require('vendor/autoload.php');
@@ -8,5 +11,14 @@ if (file_exists('vendor/autoload.php')) {
 // Requires Function composer's autoload
 require('function/vendor/autoload.php');
 
-$stdin = fgets(STDIN);
+//$stdin = (string)fgets(STDIN);
+$stdin = file_get_contents("php://stdin");
+
+if(isValidJson(($stdin))){
+    $json=json_decode($stdin);    
+    $h = (new App\Handler())->handle($json);
+    return;
+}
+
 $h = (new App\Handler())->handle($stdin);
+
